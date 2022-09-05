76d4991c083512f4fa907bed800f26891219e2a5
wagnersan7os <san7os@hotmail.com>
2022-09-05 12:24:59 -0300

    Operators

---
+++ MyChallenge-003.py
@@ -0,0 +1,47 @@
+# Criar um Script em Python que leia dois "NÚMEROS" e tente mostrar
+# a "SOMA", "SUBTRAÇÃO", "MULTIPLICAÇÃO", "DIVISÃO".
+
+print(' +++++ @WAGNERSAN7OS +++++ ')
+
+print()
+
+print('*** MY CHALLENGE 003: ***')
+
+print()
+
+nome = input('Whats your Name? ')
+
+print()
+
+print('Please enter with 2x Integer Values:')
+
+print()
+
+valor1 = int(input('Value 01.....: '))
+valor2 = int(input('Value 02.....: '))
+
+# Calculating with Operators (+, -, *, /):
+
+soma = valor1 + valor2
+sub = valor1 - valor2
+mult = valor1 * valor2
+div = valor1 / valor2
+
+print()
+
+print('*** INFORMATION COLLECTED: OPERATORS ***')
+
+print()
+
+print('*** Welcome!!! ***')
+
+print()
+
+print('+++++ {}'.format(nome), '+++++')
+
+print()
+
+print('Sum................: {}'.format(soma))
+print('Subtraction........: {}'.format(sub))
+print('Multiplication.....: {}'.format(mult))
+print('Division...........: {:.2f}'.format(div))
