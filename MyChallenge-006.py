d84424279145314453956cc9fa9f570c34f9ae97
wagnersan7os <san7os@hotmail.com>
2022-09-05 12:30:10 -0300

    Double, Triple, Source

---
+++ MyChallenge-006.py
@@ -0,0 +1,44 @@
+# Criar um Algoritimo que leia um "NÚMERO INTEIRO" e mostre na tela
+# o seu "DOBRO", o seu "TRIPLO" e a sua "RAIZ QUADRADA".
+
+print(' +++++ @WAGNERSAN7OS +++++ ')
+
+print()
+
+print('*** MY CHALLENGE 006: ***')
+
+print()
+
+nome = input('Whats your Name? ')
+
+print()
+
+print('Please enter with Integer Value:')
+
+print()
+
+valor1 = int(input('Value 01.....: '))
+
+# Calculating with Operators (*, **):
+
+dobro = valor1 * 2
+triplo = valor1 * 3
+raiz = valor1 ** (1/2)
+
+print()
+
+print('*** INFORMATION COLLECTED: DOUBLE, TRIPLE, SOURCE ***')
+
+print()
+
+print('*** Welcome!!! ***')
+
+print()
+
+print('+++++ {}'.format(nome), '+++++')
+
+print()
+
+print('Value Double.....: {}'.format(dobro))
+print('Value Triple.....: {}'.format(triplo))
+print('Value Source.....: {:.2f}'.format(raiz))
