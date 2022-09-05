6ca60bb9cc5f4344fe385d6d090543348ec582b2
wagnersan7os <san7os@hotmail.com>
2022-09-05 12:29:04 -0300

    Predecessor and Successor

---
+++ MyChallenge-005.py
@@ -0,0 +1,42 @@
+# Fazer um Programa que leia um "NÚMERO INTEIRO" e mostre na tela o seu "SUCESSOR" e seu "ANTECESSOR".
+
+print(' +++++ @WAGNERSAN7OS +++++ ')
+
+print()
+
+print('*** MY CHALLENGE 005: ***')
+
+print()
+
+nome = input('Whats your Name? ')
+
+print()
+
+print('Please enter with Integer Value:')
+
+print()
+
+valor1 = int(input('Value 01.....: '))
+
+# Calculating with Operators (+, -):
+
+ant = valor1 - 1
+suc = valor1 + 1
+
+print()
+
+print('*** INFORMATION COLLECTED: PREDECESSOR and SUCCESSOR ***')
+
+print()
+
+print('*** Welcome!!! ***')
+
+print()
+
+print('+++++ {}'.format(nome), '+++++')
+
+print()
+
+print('Value Integer.........: {}'.format(valor1))
+print('Value Predecessor.....: {}'.format(ant))
+print('Value Successor.......: {}'.format(suc))
