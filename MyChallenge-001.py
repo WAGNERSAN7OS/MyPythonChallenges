8738a338cb2dca99374a4775426d3a20479f859d
wagnersan7os <san7os@hotmail.com>
2022-09-05 12:21:52 -0300

    Hello Message

---
+++ MyChallenge-001.py
@@ -0,0 +1,20 @@
+# Criar um Script em Python que lei o "NOME" de uma pessoa e mostre uma "MENSAGEM" de "BOAS VINDAS"
+# de acordo com o valor digitado.
+
+print(' +++++ @WAGNERSAN7OS +++++ ')
+
+print()
+
+print('*** MY CHALLENGE 001: ***')
+
+print()
+
+nome = input('Whats your Name? ')
+
+print()
+
+print('*** INFORMATION COLLECTED: HELLO MESSAGE ***')
+
+print()
+
+print('Hello', nome + '!' '\nNice to meet you!!!')
