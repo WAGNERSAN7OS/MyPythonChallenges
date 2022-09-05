d804c8be3fcd627f2d62d851d4c67c52279fbb44
wagnersan7os <san7os@hotmail.com>
2022-09-05 12:26:52 -0300

    Arithmetic Operators

---
+++ MyChallenge-004x01.py
@@ -0,0 +1,52 @@
+print(' +++++ @WAGNERSAN7OS +++++ ')
+
+print()
+
+print('*** MY CHALLENGE 005: ***')
+
+print()
+
+nome = input('Whats your Name? ')
+
+print()
+
+print('Please enter with 2x Integer Values:')
+
+print()
+
+valor1 = int(input('Value 01.....: '))
+valor2 = int(input('Value 02.....: '))
+
+# Calculating with Operators (+, -, *, /, **, //, %):
+
+soma = valor1 + valor2
+sub = valor1 - valor2
+mult = valor1 * valor2
+div = valor1 / valor2
+
+pot = valor1 ** valor2
+divint = valor1 // valor2
+rest = valor1 % valor2
+
+print()
+
+print('*** INFORMATION COLLECTED: ARITHMETIC OPERATORS ***')
+
+print()
+
+print('*** Welcome!!! ***')
+
+print()
+
+print('+++++ {}'.format(nome), '+++++')
+
+print()
+
+print('Sum................: {}'.format(soma))
+print('Subtraction........: {}'.format(sub))
+print('Multiplication.....: {}'.format(mult))
+print('Division...........: {:.2f}'.format(round(div)))
+
+print('Potency............: {}'.format(pot))
+print('Whole Division.....: {}'.format(divint))
+print('Rest...............: {}'.format(rest))
