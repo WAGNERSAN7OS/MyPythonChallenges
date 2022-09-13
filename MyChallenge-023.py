8729da1aa07d6403f8e99d420777634a3acd0f17
wagnersan7os <san7os@hotmail.com>
2022-09-13 14:48:33 -0300

    Separating Digits of a Number

---
+++ MyChallenge-023.py
@@ -0,0 +1,55 @@
+# Fazer um PROGRAMA que "LEIA" um "NÚMERO" de 0 até 9999 e mostra na "TELA" cada um dos Dígitos separados.
+
+# Exemplo:
+
+# Digite um Número: 1979
+# Unidade.....: 4
+# Dezena......: 3
+# Centena.....: 8
+# Milhar......: 1
+
+print(' +++++ @WAGNERSAN7OS +++++ ')
+
+print()
+
+print('*** MY CHALLENGE 023: ***')
+
+print()
+
+nome = input('Whats your Name? ')
+
+print()
+
+num = int(input('Enter the Number here: '))
+
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print()
+
+print('*** INFORMATION COLLECTED: SEPARATING DIGITS OF A NUMBER ***')
+
+print()
+
+print('*** Welcome!!! ***')
+
+print()
+
+print('+++++ {}'.format(nome), '+++++')
+
+print()
+
+print('*** NUMBERS ***')
+
+print()
+
+print('Analyzing the reported number: {}'.format(num))
+
+print()
+
+print('Unit.........: {}'.format(u))
+print('Ten..........: {}'.format(d))
+print('Hundred......: {}'.format(c))
+print('Thousend.....: {}'.format(m))
