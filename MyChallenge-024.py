67c69aba4a5c1306f52bdd25e167e982c2343df5
wagnersan7os <san7os@hotmail.com>
2022-09-14 14:28:48 -0300

    Checking First Name

---
+++ MyChallenge-024.py
@@ -0,0 +1,37 @@
+# Criar um "PROGRAMA" que "LEIA" o "NOME DE UMA CIDADE" e
+# verifique se ela começa ou não com o Nome: "SANTOS"
+
+print(' +++++ @WAGNERSAN7OS +++++ ')
+
+print()
+
+print('*** MY CHALLENGE 024: ***')
+
+print()
+
+nome = input('Whats your Name? ')
+
+print()
+
+cidade = str(input('Enter City Name: ')).strip()
+
+print()
+
+print('*** INFORMATION COLLECTED: CHECKING THE FIRST LETTERS OF A WORD ***')
+
+print()
+
+print('*** Welcome!!! ***')
+
+print()
+
+print('+++++ {}'.format(nome), '+++++')
+
+print()
+
+print('*** FIRST WORD ***')
+
+print()
+
+print('Name of the City.....: {}'.format(cidade))
+print('First Name...........:', cidade[:6].upper() == cidade.upper().split()[0])
